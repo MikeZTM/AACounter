4c2faa4fd1dbd69556a43ed65887424a11dbc5f7
MikeZTM <liyingzhuomike@gmail.com>
2015-07-31 01:33:16 -0400

    first demo

--- AACounter/ViewController.swift
+++ AACounter/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plusClick(sender: AnyObject) {
+        let newItem = NSEntityDescription.insertNewObjectForEntityForName("CountItem", inManagedObjectContext: self.managedObjectContext!) as! CountItem
+        newItem.time=NSDate()
+        newItem.device="iPhone"
+        newItem.lat=50.0;
+        newItem.long=55.5;
+    }
 
+    @IBAction func printNow(sender: AnyObject) {
+        // Create a new fetch request using the LogItem entity
+        let fetchRequest = NSFetchRequest(entityName: "CountItem")
+        
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd-hh:mm" //format style. Browse online to get a format that fits your needs.
+        
+        // Execute the fetch request, and cast the results to an array of LogItem objects
+        if let fetchResults = managedObjectContext!.executeFetchRequest(fetchRequest, error: nil) as? [CountItem] {
+            
+            var dateString = dateFormatter.stringFromDate(fetchResults[0].time)
+            // Create an Alert, and set it's message to whatever the itemText is
+            let alert = UIAlertController(title: fetchResults[0].device,
+                message: dateString, preferredStyle: UIAlertControllerStyle.Alert)
+            let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in
+                //Do some stuff
+            }
+            alert.addAction(cancelAction)
+            
+            // Display the alert
+            self.presentViewController(alert,
+                animated: true,
+                completion: nil)
+        }
+    }
 }
 
