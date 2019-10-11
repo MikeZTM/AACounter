cb1e9bd9a4c708a1c7a762358fb1b46f3b2b241f
Mike <mike@limosys.com>
2019-10-11 17:17:33 -0400

    remove NSToolbar

--- AACounter/ViewController.swift
+++ AACounter/ViewController.swift
@@ -25,9 +25,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             self.plusClick(self)
             aacDelegate.shortcut=false
         }
-        #if targetEnvironment(macCatalyst)
-          let toolbar = NSToolbar(identifier: "MyToolbar")
-        #endif
         // Do any additional setup after loading the view, typically from a nib.
     }
     
